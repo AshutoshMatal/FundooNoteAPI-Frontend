106b9675d2e54d5567ef2b0f5f8f46869fe6c658
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-05-12 22:09:01 +0530

    MODIFIED:Added matsnackbar to display the message

--- FirstApp/src/app/login/login.component.ts
+++ FirstApp/src/app/login/login.component.ts
@@ -1,20 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import{FormGroup,FormControl,Validators}from '@angular/forms';
 import {LoginService}from'../shared/login.service';
-
-
+import { UserLogin } from '../user-login';
+import {MatSnackBar} from '@angular/material/snack-bar';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+ 
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
   title='Welcome to FundooNoteAPI';
-  constructor(public service:LoginService) { }
-    
-
+  constructor(public service:LoginService,
+    private matsnackBar:MatSnackBar
+    ) { }
+  
   ngOnInit() {
   }
+  userlogin: UserLogin=new UserLogin("","");
+  message:any;
+
+  public loginNow(){
+    let resp=this.service.doLogin(this.userlogin);
+    resp.subscribe((data)=>
+    {
+      console.log(data);
+      if(data!=null)
+      {
+        this.matsnackBar.open('Login Successfully', 'LoginIn',{
+          duration:1000,
+          verticalPosition:'top'
+        });
+      }else{
+        this.matsnackBar.open("Enter valid data",'Login Failed',{
+          duration:1000,
+          verticalPosition:'top'
+        });
+      }   
+},
+error=>{
+  console.log('Error',error);
+}
+    );
+}
 
 }
