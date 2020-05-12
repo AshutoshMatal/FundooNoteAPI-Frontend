9af73604952964669f704d82f5802092d6b91ec4
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-05-13 00:10:10 +0530

    MODIFIED:Added snackbar to register to show the message

--- FirstApp/src/app/register/register.component.ts
+++ FirstApp/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import { RegisterService } from '../shared/register.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -11,13 +13,32 @@ export class RegisterComponent implements OnInit {
 user: User=new User("","","","","",0);
   message:any;
 
-  constructor(public service:RegisterService) { }
+  constructor(public service:RegisterService,
+    private matsnackBar:MatSnackBar) { }
   
   ngOnInit() {
   }
   public registerNow(){
+    
     let resp=this.service.doRegistration(this.user);
-    resp.subscribe((data)=>this.message=data);
-      }
-
+    resp.subscribe((data)=>
+    {
+      console.log(data);
+      if(data!=null)
+      {
+        this.matsnackBar.open('Registration Successfully', 'RegisterIn',{
+          duration:5000,
+          verticalPosition:'top'
+        });
+      }else{
+        this.matsnackBar.open("Enter valid data",'Registration Failed',{
+          duration:5000
+        });
+      }   
+},
+error=>{
+  console.log('Error',error);
+}
+    );
+}
 }
