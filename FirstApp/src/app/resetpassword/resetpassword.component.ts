ded04d856094feba577d6a7fd738573f1cf1f80a
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-05-13 01:23:41 +0530

    MODIFIED:Added snackbar to show the message after submitting the form

--- FirstApp/src/app/resetpassword/resetpassword.component.ts
+++ FirstApp/src/app/resetpassword/resetpassword.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ResetpasswordService } from '../shared/resetpassword.service';
+import {ResetpasswordService}from'../shared/resetpassword.service';
+import { UserResetPassword } from '../user-reset';
+import {MatSnackBar} from '@angular/material/snack-bar';
 @Component({
   selector: 'app-resetpassword',
   templateUrl: './resetpassword.component.html',
@@ -7,11 +9,36 @@ import { ResetpasswordService } from '../shared/resetpassword.service';
 })
 export class ResetpasswordComponent implements OnInit {
 
-  constructor(public service:ResetpasswordService) { }
-  
-  
+  constructor(public service:ResetpasswordService,
+    private matsnackBar:MatSnackBar) { }
 
   ngOnInit(): void {
   }
+  userreset: UserResetPassword=new UserResetPassword("","");
+  message:any;
 
+  
+  public ResetNow(){
+    let resp=this.service.doReset(this.userreset);
+    resp.subscribe((data)=>
+    {
+      console.log(data);
+      if(data!=null)
+      {
+        this.matsnackBar.open('Resetpassword', 'success',{
+          duration:1000,
+          verticalPosition:'top'
+        });
+      }else{
+        this.matsnackBar.open("Enter valid data",'Login Failed',{
+          duration:1000,
+          verticalPosition:'top'
+        });
+      }   
+},
+error=>{
+  console.log('Error',error);
+}
+    );
+  }
 }
