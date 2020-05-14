d93c6500f1e1c5489fc6e67994697c25ce003dbc
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-05-15 01:44:01 +0530

    MODIFIED: Added Http client and resetpassword method

--- FirstApp/src/app/shared/resetpassword.service.ts
+++ FirstApp/src/app/shared/resetpassword.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import{FormGroup,FormControl,Validators}from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class ResetpasswordService {
 
-  constructor() { }
+  constructor(public http:HttpClient) { }
+  public doReset(userreset){
+    return this.http.put("http://localhost:8080/user/reset",userreset,{responseType:'text' as 'json'});
+  } 
   form:FormGroup=new FormGroup({ 
-    $key:new FormControl(null),
+    $key:new FormControl(),
     password:new FormControl('',Validators.required),
-   confirmpassword:new FormControl('',Validators.required),
+   confirmPassword:new FormControl('',Validators.required),
   
   });
 }
