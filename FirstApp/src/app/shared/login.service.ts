449a24af706dbf89f7130ca8ac79c122fa90fd96
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-05-15 01:41:39 +0530

    MODIFIED: Added Http client and login method

--- FirstApp/src/app/shared/login.service.ts
+++ FirstApp/src/app/shared/login.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import{FormGroup,FormControl,Validators}from '@angular/forms';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(public http:HttpClient) { }
+  public doLogin(userlogin){
+    return this.http.post("http://localhost:8080/user/login",userlogin,{responseType:'text' as 'json'});
+  }
+
+
   form:FormGroup=new FormGroup({ 
-    $key:new FormControl(null),
-    username:new FormControl('',Validators.required),
+    $key:new FormControl(),
+    emailId:new FormControl('',Validators.required),
     password:new FormControl('',Validators.required),
   
   });
