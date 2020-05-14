5baecb4341b027bb4e9c42f49938aa2dedbe8423
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-05-15 00:09:05 +0530

    MODIFIED: Proper Intendation

---
+++ FirstApp/src/app/dto/user.ts
@@ -0,0 +1,18 @@
+export class User {
+    username: String;
+    name: String;
+    password: String;
+    emailId: String;
+    country: String;
+    mobileNo: number;
+    constructor(
+        username: string,
+        name: string,
+        password: string,
+        emailId: string,
+        country: string,
+        mobileNo: number
+    ) {
+
+    }
+}
