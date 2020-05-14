924a13c80f87f94dd8e768ad4e2fd745c5560609
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-05-15 01:43:18 +0530

    MODIFIED: Added Http client and forgetpassword method

--- FirstApp/src/app/shared/forgetpassword.service.ts
+++ FirstApp/src/app/shared/forgetpassword.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
 import{FormGroup,FormControl,Validators}from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class ForgetpasswordService {
 
-  constructor() { }
+
+ 
+  constructor(public http:HttpClient) { }
+  public doForget(userforget){
+    return this.http.post("http://localhost:8080/user/forget",userforget,{responseType:'text' as 'json'});
+  }
+
   form:FormGroup=new FormGroup({
-    $key:new FormControl(null),
+    $key:new FormControl(),
     emailId:new FormControl('',Validators.required),
-    password:new FormControl('',Validators.required),
+    mobileNo:new FormControl('',Validators.required),
   
   });
   
