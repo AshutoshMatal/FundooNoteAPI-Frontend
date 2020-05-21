6c3bb3d87ae4b161552a40c7b1f4cc1cb25cb141
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-05-22 03:30:39 +0530

    MODIFIED:Added and save token to local storage

--- FirstApp/src/app/login/login.component.ts
+++ FirstApp/src/app/login/login.component.ts
@@ -1,48 +1,112 @@
 import { Component, OnInit } from '@angular/core';
-import {LoginService}from'../shared/login.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators ,FormBuilder } from '@angular/forms';
+import { ServiceService } from '../shared/service.service';
 import { UserLogin } from '../Dto/user-login';
-import {MatSnackBar} from '@angular/material/snack-bar';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
- 
+
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  title='Welcome to FundooNoteAPI';
-  constructor(public loginservice:LoginService,
-    private matsnackBar:MatSnackBar
-    ) { }
+  message: any;
   
-  ngOnInit() {
-  }
-  userlogin: UserLogin=new UserLogin("","");
-  message:any;
+  loginForm: FormGroup;
 
-  public loginNow(){
-    let response=this.loginservice.doLogin(this.userlogin);
-    response.subscribe((data)=>
-    {
-      console.log(data);
-      if(data!=null)
-      {
-        this.matsnackBar.open('Login Successfully', 'LoginIn',{
-          duration:1000,
-          verticalPosition:'top'
+  login:UserLogin = new UserLogin();
+  
+  constructor(private matsnackbar: MatSnackBar, private service: ServiceService, private router: Router) { }
+  emailId = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(4)]);
+
+  ngOnInit() {
+
+  }
+  loginNow() {
+    this.login.emailId = this.emailId.value;
+    this.login.password = this.password.value;
+    this.service.login(this.login).subscribe(
+    (response:any) => {
+
+      //Token has been added in the local storage 
+      localStorage.setItem('token', response.data);
+      console.log(response);
+      localStorage.setItem('email', this.emailId.value);
+          this.matsnackbar.open(response.message, "Login Succesfull", {duration:5000})
+          this.router.navigate(['dashboard']);
+    },
+    (error:any)=> {
+      this.matsnackbar.open(error.error.message, "Login Failed", {duration:5000})
+    
+    }
+   );
+  }
+}
+
+  /*
+    console.log(this.emailId.value);
+    const user = {
+      emailId: this.emailId.value,
+      password: this.password.value
+    };
+    this.service.login(user).subscribe(
+     data => {
+       console.log(data);
+       
+       if (data.statuscode == 200) {
+
+        console.log('All data===>'+data);
+                this.matsnackbar.open(' Login Successfully ', 'LogIn', {
+           duration: 2000
+         });     
+         this.router.navigate(['dashboard']);
+         localStorage.setItem("login", data.token);
+         localStorage.setItem("emailId", user.emailId);
+          
+     }
+     else {
+      this.matsnackbar.open("Enter valid data", 'Login Failed', {
+        duration: 2000
+      });
+
+    }
+  },
+      (error) => {
+        console.log('Error', error);
+        this.matsnackbar.open(error.message,"login failed",{
+          duration:5000})
         });
       }
-      else{
-        this.matsnackBar.open("Enter valid data",'Login Failed',{
-          duration:1000,
-          verticalPosition:'top'
-        });
-      }   
-},
-error=>{
-  console.log('Error',error);
-}
-    );
-}
+  /*
+  userlogin: UserLogin = new UserLogin("", "");
+    message: any;
+  
+    public loginNow() {
+      let response = this.loginservice.doLogin(this.userlogin);
+      response.subscribe((data) => {
+        console.log(data);
+        if (data != null) {
+          this.matsnackBar.open('Login Successfully', 'Success', {
+            duration: 1000,
+            verticalPosition: 'top'
+          });
+          this.router.navigate(['dashboard']);
+          
+        }
+        else {
+          this.matsnackBar.open("Enter valid data", 'Login Failed', {
+            duration: 1000,
+            verticalPosition: 'top'
+          });
+        }
+      },
+        error => {
+          console.log('Error', error);
+        }
+      );
+    }
+  
+  }*/
 
-}
